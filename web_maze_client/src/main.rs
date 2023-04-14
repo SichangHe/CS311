743d4cc5255ed0b004d7f416c10ae2cde072f207
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-04-14 21:16:22 +0800

    hardcoded request that hang on body referencing https://doc.rust-lang.org/stable/book/ch20-01-single-threaded.html

--- web_maze_client/src/main.rs
+++ web_maze_client/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    io::{prelude::*, BufReader},
+    net::TcpStream,
+};
+
+type ResultDyn<T> = Result<T, Box<dyn Error>>;
+
+fn main() -> ResultDyn<()> {
+    let host = "127.0.0.1:4000";
+    let mut stream = TcpStream::connect(host)?;
+    let to_write = format!(
+        "GET /api/list HTTP/1.1\r
+Host: {host}\r
+\r
+"
+    );
+    println!("{to_write}");
+    stream.write_all(to_write.as_bytes())?;
+    let reader = BufReader::new(stream);
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+    Ok(())
 }
