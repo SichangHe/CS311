33ece7aa27f27f9f2085362fcb782e428fd06ae0
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-05-02 22:18:48 +0800

    remove peer routes on timeout

--- udprip/src/route.rs
+++ udprip/src/route.rs
@@ -4,6 +4,7 @@ use std::{cmp::Ordering, collections::BTreeMap, net::IpAddr, time::Duration};
 use tokio::{
     spawn,
     sync::mpsc::{channel, Receiver, Sender},
+    task::JoinHandle,
     time::timeout,
 };
 
@@ -15,34 +16,53 @@ pub async fn manage(
     senders: Senders,
     mut cmd_receiver: Receiver<Route>,
 ) {
+    let time_out = period * 4;
     let mut accept = BTreeMap::new();
-    let mut peer: BTreeMap<_, BTreeMap<_, _>> = BTreeMap::new();
+    let mut peers = Peers::new();
     let mut next = BTreeMap::new();
     let (notify_sender, notify_receiver) = channel(2);
     let _time_notify = spawn(time_notify(period, senders.clone(), notify_receiver));
     while let Some(route) = cmd_receiver.recv().await {
-        debug!("Route: {route:#?}");
+        debug!("Route: {route:?}");
         match route {
             Route::Add { ip, weight } => {
                 accept.insert(ip, weight);
-                debug!("`accept` is now {accept:#?}");
-                next = calculate_next(&accept, &peer);
+                debug!("`accept` is now {accept:?}");
+                next = calculate_next(&accept, &peers.peers);
                 notify(addr, &accept, &next, &senders, &notify_sender).await;
             }
             Route::Del { ip } => {
                 accept.remove(&ip);
-                debug!("`accept` is now {accept:#?}");
-                next = calculate_next(&accept, &peer);
+                debug!("`accept` is now {accept:?}");
+                next = calculate_next(&accept, &peers.peers);
                 notify(addr, &accept, &next, &senders, &notify_sender).await;
             }
             Route::Update { source, distances } => {
-                peer.insert(source, distances);
-                next = calculate_next(&accept, &peer);
+                peers
+                    .insert(source, distances, time_out, senders.clone())
+                    .await;
+                next = calculate_next(&accept, &peers.peers);
             }
             Route::Forward { msg } => send(&next, &msg.destination.clone(), msg, &senders).await,
             Route::Notify => notify(addr, &accept, &next, &senders, &notify_sender).await,
+            Route::DelPeer { ip } => {
+                debug!("Remove dead peer {ip}.");
+                peers.remove(&ip).await;
+                next = calculate_next(&accept, &peers.peers);
+                debug!("`next` is now {next:?}.");
+            }
         }
     }
+}
+
+async fn timeout_peer(
+    ip: IpAddr,
+    time_out: Duration,
+    senders: Senders,
+    mut peer_receiver: Receiver<()>,
+) {
+    while (timeout(time_out, peer_receiver.recv()).await).is_ok() {}
+    senders.route(Route::DelPeer { ip }).await;
 }
 
 async fn time_notify(period: Duration, senders: Senders, mut notify_receiver: Receiver<()>) {
@@ -80,6 +100,7 @@ async fn notify(
     senders: &Senders,
     notify_sender: &Sender<()>,
 ) {
+    // Reset notify timer.
     notify_sender
         .send(())
         .await
@@ -139,6 +160,48 @@ fn calculate_next(
     next
 }
 
+struct Peers {
+    peers: BTreeMap<IpAddr, BTreeMap<IpAddr, usize>>,
+    timeout_peers: BTreeMap<IpAddr, JoinHandle<()>>,
+    peer_senders: BTreeMap<IpAddr, Sender<()>>,
+}
+
+impl Peers {
+    fn new() -> Self {
+        Self {
+            peers: BTreeMap::new(),
+            timeout_peers: BTreeMap::new(),
+            peer_senders: BTreeMap::new(),
+        }
+    }
+
+    async fn insert(
+        &mut self,
+        ip: IpAddr,
+        distances: BTreeMap<IpAddr, usize>,
+        time_out: Duration,
+        senders: Senders,
+    ) {
+        self.peers.insert(ip, distances);
+        match self.peer_senders.get(&ip) {
+            // Reset timeout_peer timeout.
+            Some(peer_sender) => peer_sender.send(()).await.expect("Peer receiver closed."),
+            None => {
+                let (peer_sender, peer_receiver) = channel(1);
+                let process = spawn(timeout_peer(ip, time_out, senders, peer_receiver));
+                self.timeout_peers.insert(ip, process);
+                self.peer_senders.insert(ip, peer_sender);
+            }
+        }
+    }
+
+    async fn remove(&mut self, ip: &IpAddr) {
+        self.timeout_peers.remove(ip);
+        self.peer_senders.remove(ip);
+        self.peers.remove(ip);
+    }
+}
+
 #[derive(Debug)]
 pub enum Route {
     Add {
@@ -156,4 +219,7 @@ pub enum Route {
         msg: Message,
     },
     Notify,
+    DelPeer {
+        ip: IpAddr,
+    },
 }
